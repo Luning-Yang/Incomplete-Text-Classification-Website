620457cebdcddfb605c495f71623e5b29794abb8
Luning Yang <luningyang@Lunings-Air.lan>
2023-02-26 01:33:32 -0800

    method first attempt

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -13,23 +13,12 @@ const Portfolio: FC = memo(() => {
   return (
     <Section className="bg-neutral-800" sectionId={SectionId.Portfolio}>
       <div className="flex flex-col gap-y-8">
-        <h2 className="self-center text-xl font-bold text-white">Methods Overview</h2>
-       {/* <div className=" w-full columns-2 md:columns-3 lg:columns-4">
-          {portfolioItems.map((item, index) => {
-            const {title, image} = item;
-            return (
-              <div className="pb-6" key={`${title}-${index}`}>
-                <div
-                  className={classNames(
-                    'relative h-max w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
-                  )}>
-                  <Image alt={title} layout="responsive" placeholder="blur" src={image} />
-                  <ItemOverlay item={item} />
-                </div>
-              </div>
-            );
-          })}
-        </div>*/}
+        <h2 className="self-center text-xl font-bold uppercase text-white">Methods Overview</h2>
+      </div>
+
+      <div className="flex flex-col gap-y-8">
+      <h4 className="self-left text-xl font-bold text-white">1. Baseline Model</h4>
+      <p className="self-left text-sm font-light text-white">Our baseline model is </p>
       </div>
     </Section>
   );
