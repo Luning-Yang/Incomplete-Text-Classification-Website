1e5580635755ee2cebce2b4e37aab1c735465531
Luning Yang <luningyang@Lunings-Air.lan>
2023-02-26 21:47:19 -0800

    deploy11111

--- next.config.js
+++ next.config.js
@@ -20,11 +20,11 @@ const nextConfig = {
   pageExtensions: ['tsx', 'mdx', 'ts'],
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
-  svgo: {
-    multipass: true,
-    plugins: ['removeDimensions'],
-  },
-  strictMode: true,
+  // svgo: {
+  //   multipass: true,
+  //   plugins: ['removeDimensions'],
+  // },
+  // strictMode: true,
   swcMinify: true,
   trailingSlash: false,
   images: {
